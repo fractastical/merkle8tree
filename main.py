988921047c959c54b392772570480b8a4f9cd38c
Joel Dietz <jdietz+github@gmail.com>
2023-09-10 20:25:00 -0700

    readd visuals

--- main.py
+++ main.py
@@ -1,143 +1,133 @@
 import hashlib
 import random
-import matplotlib.pyplot as plt
 import numpy as np
 from pympler import asizeof
+import matplotlib.pyplot as plt
 
-# Define basic structures
 
 def compute_hash(data):
     return hashlib.sha256(data.encode('utf-8')).hexdigest()
 
+
 class Point3D:
-    def __init__(self, x, y, z, data):
+    def __init__(self, x, y, z, data=""):
         self.x = x
         self.y = y
         self.z = z
         self.data = data
-        self.merkle_root = None  # Add attribute to store merkle root
-        self.storage_identifier = None  # Add attribute to store storage identifier
 
-class OctreeNode:
-    def __init__(self, bounds):
+
+class Octree:
+    def __init__(self, bounds, depth=0):
         self.bounds = bounds
         self.data = None
-        self.children = None
+        self.children = [None for _ in range(8)]
+        self.depth = depth
         self.hash = None
 
-    def is_leaf(self):
-        return self.children is None
-
-    def compute_node_hash(self):
-        # For leaf nodes
-        if self.data:
-            self.hash = compute_hash(self.data.data)
-        # For non-leaf nodes
-        elif self.children:
-            combined_hashes = ''.join(child.hash for child in self.children if child and child.hash)
-            self.hash = compute_hash(combined_hashes)
-
-    def get_child_index(self, point):
-        mid_x, mid_y, mid_z = self.bounds.center
-        index = 0
-        if point.x > mid_x:
-            index |= 1
-        if point.y > mid_y:
-            index |= 2
-        if point.z > mid_z:
-            index |= 4
-        return index
-
-    def depth_of_point(self, point):
-        depth = 0
-        node = self
-        while node and (node.data is None or isinstance(node.data, Point3D)):
-            idx = node.get_child_index(point)
-            node = node.children[idx] if node.children else None
-            depth += 1
-        return depth
-
     def insert(self, point):
-        # Ensure the point belongs within the node's bounds.
-        if not self.bounds.contains_point(point.x, point.y, point.z):
+        if not self.bounds.contains_point(point):
             return False
 
-        # If node is a leaf and empty, store the point.
-        if self.is_leaf() and self.data is None:
+        if self.depth == Octree.MAX_DEPTH:
             self.data = point
-            self.compute_node_hash()
+            self.hash = compute_hash(point.data)
             return True
 
-        # If node is a leaf but has a point, subdivide.
-        if self.is_leaf() and self.data:
-            existing_point = self.data
-            self.data = None
-            self.subdivide()
-            self.children[self.get_child_index(existing_point)].insert(existing_point)
+        idx = self.bounds.get_child_index(point)
+        if not self.children[idx]:
+            new_bounds = self.bounds.get_child_bounds(idx)
+            self.children[idx] = Octree(new_bounds, self.depth + 1)
 
-        # Insert the new point into the correct child node.
-        idx = self.get_child_index(point)
-        if self.children[idx].insert(point):
+        child_inserted = self.children[idx].insert(point)
+        if child_inserted:
             self.compute_node_hash()
-            return True
+        return child_inserted
 
-        return False
+    def compute_node_hash(self):
+        combined_hashes = ''.join(child.hash for child in self.children if child and child.hash)
+        self.hash = compute_hash(combined_hashes)
 
-    def textual_representation(self, indent=0):
-        def format_data(data):
-            if isinstance(data, Point3D):
-                return f"{data.data} ({indent})"
-            else:
-                return '...'
+    def visualize(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        xs, ys, zs = [], [], []
+        self.collect_points(xs, ys, zs)
+        ax.scatter(xs, ys, zs)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
 
-        prefix = '  ' * indent
-        output = ""
+    def collect_points(self, xs, ys, zs):
+        if self.data:
+            xs.append(self.data.x)
+            ys.append(self.data.y)
+            zs.append(self.data.z)
+        for child in self.children:
+            if child:
+                child.collect_points(xs, ys, zs)
 
-        if self.data or (self.children and any(child for child in self.children)):
-            output += f"{prefix}{format_data(self.data)}\n"
-            if self.children:
-                for child in self.children:
-                    if child:
-                        output += child.textual_representation(indent + 1)
-        return output
 
-    def subdivide(self):
-        x, y, z = self.bounds.center
-        d = self.bounds.dimension / 4.0
-        self.children = [
-            OctreeNode(OctreeBoundingBox(x - d, x, y - d, y, z - d, z)),
-            OctreeNode(OctreeBoundingBox(x, x + d, y - d, y, z - d, z)),
-            OctreeNode(OctreeBoundingBox(x - d, x, y, y + d, z - d, z)),
-            OctreeNode(OctreeBoundingBox(x, x + d, y, y + d, z - d, z)),
-            OctreeNode(OctreeBoundingBox(x - d, x, y - d, y, z, z + d)),
-            OctreeNode(OctreeBoundingBox(x, x + d, y - d, y, z, z + d)),
-            OctreeNode(OctreeBoundingBox(x - d, x, y, y + d, z, z + d)),
-            OctreeNode(OctreeBoundingBox(x, x + d, y, y + d, z, z + d))
-        ]
-
-class OctreeBoundingBox:
+class OctreeBounds:
     def __init__(self, x1, x2, y1, y2, z1, z2):
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
-        self.z1 = z1
-        self.z2 = z2
-        self.center = ((x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2)
-        self.dimension = max(x2 - x1, y2 - y1, z2 - z1)
+        self.x1, self.x2 = x1, x2
+        self.y1, self.y2 = y1, y2
+        self.z1, self.z2 = z1, z2
 
-    def contains_point(self, x, y, z):
-        return self.x1 <= x < self.x2 and self.y1 <= y < self.y2 and self.z1 <= z < self.z2
+    def contains_point(self, point):
+        return self.x1 <= point.x < self.x2 and self.y1 <= point.y < self.y2 and self.z1 <= point.z < self.z2
 
-# Test the Octree
+    def get_child_index(self, point):
+        mid_x = (self.x1 + self.x2) / 2
+        mid_y = (self.y1 + self.y2) / 2
+        mid_z = (self.z1 + self.z2) / 2
 
-# Initialize the octree with bounding box spanning from 0 to 100 in all dimensions.
-octree = OctreeNode(OctreeBoundingBox(0, 100, 0, 100, 0, 100))
+        index = 0
+        if point.x >= mid_x:
+            index |= 1
+        if point.y >= mid_y:
+            index |= 2
+        if point.z >= mid_z:
+            index |= 4
 
-# Insert some random data
-points = [Point3D(random.randint(0, 100), random.randint(0, 100), random.randint(0, 100), f"Point-{i}") for i in range(100)]
+        return index
+
+    def get_child_bounds(self, index):
+        mid_x = (self.x1 + self.x2) / 2
+        mid_y = (self.y1 + self.y2) / 2
+        mid_z = (self.z1 + self.z2) / 2
+
+        if index & 1:
+            x1, x2 = mid_x, self.x2
+        else:
+            x1, x2 = self.x1, mid_x
+
+        if index & 2:
+            y1, y2 = mid_y, self.y2
+        else:
+            y1, y2 = self.y1, mid_y
+
+        if index & 4:
+            z1, z2 = mid_z, self.z2
+        else:
+            z1, z2 = self.z1, mid_z
+
+        return OctreeBounds(x1, x2, y1, y2, z1, z2)
+
+
+Octree.MAX_DEPTH = 4  # Adjust as necessary
+
+# Testing the octree with random data
+points = [Point3D(random.uniform(0, 100), random.uniform(0, 100), random.uniform(0, 100), str(random.randint(0, 1e6))) for _ in range(1000)]
+octree = Octree(OctreeBounds(0, 100, 0, 100, 0, 100))
+
 for point in points:
     octree.insert(point)
 
-# Print the octree
-print(octree.textual_representation())
+# Visualizing
+octree.visualize()
+
+# Computing size
+tree_size = asizeof.asizeof(octree)
+print(f"Size of the Octree: {tree_size} bytes")
